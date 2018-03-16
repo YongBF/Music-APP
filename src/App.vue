ffe3dbcacb36544eff18374c071ec09a95629939
YongBF <1727132050@qq.com>
2018-03-16 23:59:58 +0800

    头部导航制作

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    hello music!
+    <MHeader/>
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MHeader
+  }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
-
-  #app
-    color:$color-theme
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
 
+fastclick.attach(document.body)
+/* 将fastclick应用于body解决300ms延迟 */
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
