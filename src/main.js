a3047c38b2e1ab5706c28a3e7d2d2202b169c606
YongBF <1727132050@qq.com>
2018-03-22 22:50:41 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,9 +4,13 @@ import App from './App'
 import router from './router'
 import 'common/stylus/index.styl'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 fastclick.attach(document.body)
 /* 将fastclick应用于body解决300ms延迟 */
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
