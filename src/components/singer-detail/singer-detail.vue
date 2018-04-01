9c4bf2bbd651eab52d46ef74e855946626631f29
YongBF <1727132050@qq.com>
2018-04-01 22:53:14 +0800

    歌曲数据的抓取

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,8 +8,14 @@
 import {mapGetters} from 'vuex'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
@@ -26,9 +32,18 @@ export default {
       }
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
         }
       })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        let {musicData} = item
+        ret.push(createSong(musicData))
+      })
+      return ret
     }
   }
 }
